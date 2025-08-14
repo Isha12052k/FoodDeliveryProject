dc860e33b2d2dc721fde60e173350dc4bdd32926
Isha <ishanaik2k12@gmail.com>
2025-08-14 10:25:36 +1000

    FDSI-31 added menu route

---
+++ backend/routes/menuItemRoutes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const { protect } = require('../middleware/authMiddleware');
+const {
+  createMenuItem,
+  getMenuItems
+} = require('../controllers/menuItemController');
+const upload = require('../middleware/uploadMiddleware'); // For image uploads
+
+router.route('/:restaurantId/menu')
+  .get(getMenuItems)
+  .post(protect, upload.single('image'), createMenuItem);
+
+module.exports = router;
