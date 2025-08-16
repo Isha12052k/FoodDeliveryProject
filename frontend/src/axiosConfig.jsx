4073695814c3734066a27dbed100e1591ca4b881
Isha <ishanaik2k12@gmail.com>
2025-08-16 21:36:06 +1000

    base URL changed again, should work now

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   //baseURL: 'http://localhost:5000/api', // local
-  baseURL: 'http://3.25.58.68:5001', // live
+  baseURL: 'http://3.25.58.68:5000', // live
   headers: { 'Content-Type': 'application/json' },
 });
 
