b533a2569bf9d8a256b521c949d0019f6669d56e
Isha <ishanaik2k12@gmail.com>
2025-08-16 21:15:01 +1000

    changed baseURL

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000/api', // local
-  //baseURL: 'http://3.26.96.188:5001', // live
+  //baseURL: 'http://localhost:5000/api', // local
+  baseURL: 'http://3.25.58.68:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
 
