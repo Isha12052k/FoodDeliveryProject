434548a3d92e063f4aa5dfc9c7f276ea1f647834
Isha <ishanaik2k12@gmail.com>
2025-08-16 23:13:44 +1000

    public url changes

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   //baseURL: 'http://localhost:5000/api', // local
-  baseURL: 'http://3.25.58.68:5000', // live
+  baseURL: 'http://13.211.176.168:5000', // live
   headers: { 'Content-Type': 'application/json' },
 });
 
